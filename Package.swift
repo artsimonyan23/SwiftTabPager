3193f68016257a6a4c178ae034e923fc2ce86a4c
ArtS <arthur.simonyan@buymie.eu>
2026-05-29 11:14:09 +0400

    no message

--- Package.swift
+++ Package.swift
@@ -1,5 +1,23 @@
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
-  name: "SwiftTabPager"
+    name: "SwiftTabPager",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "SwiftTabPager",
+            targets: ["SwiftTabPager"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "SwiftTabPager",
+            path: "Source",
+            exclude: ["Info.plist"]
+        )
+    ],
+    swiftLanguageVersions: [.v5]
 )
